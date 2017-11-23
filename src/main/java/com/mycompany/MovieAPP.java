4bb9f5d1d458d4de041f6c5d5946f1bf8f175942
MaciejJava <mac.laurek@gmail.com>
2017-11-23 17:35:18 +0100

    Add files via upload

---
+++ src/main/java/com/mycompany/MovieAPP.java
@@ -0,0 +1,17 @@
+package com.mycompany;
+
+import com.mycompany.entity.view.Window;
+
+public class MovieAPP {
+
+    public static void main(String[] args) {
+
+        Window main = new Window();
+
+    }
+}
+
+
+
+
+
